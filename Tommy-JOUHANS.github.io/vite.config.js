4c1e983117f028d7e8e0b41a60eec1247043a033
Tommy JOUHANS <12119@holbertonstudents.com>
2026-05-25 15:49:59 +0200

    site internet

--- Tommy-JOUHANS.github.io/vite.config.js
+++ Tommy-JOUHANS.github.io/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Pour GitHub Pages sur le repo username.github.io, base = '/'
-  base: '/',
+  // Repo projet GitHub Pages → base = '/NomDuRepo/'
+  // Change 'Tommy-JOUHANS' si ton repo a un autre nom
+  base: '/Tommy-JOUHANS/',
 })
