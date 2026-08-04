2bdb46888315162a83683cdad695088bf509c810
Tommy JOUHANS <12119@holbertonstudents.com>
2026-08-04 23:48:11 +0200

    correction de la virgule dans recommandation

--- Tommy-JOUHANS.github.io/src/data/recommendation.js
+++ Tommy-JOUHANS.github.io/src/data/recommendation.js
@@ -1,6 +1,6 @@
 export const RECOMMENDATION = {
   title: 'Lettre de recommandation de Fabien Chavonet',
-  place: 'Coach Software Engineer – Holberton School Dijon',
+  place: 'Coach Software Engineer, Holberton School Dijon',
   image: 'lettre-recommandation.jpeg',
   imageAlt: 'Lettre de recommandation professionnelle',
 }
